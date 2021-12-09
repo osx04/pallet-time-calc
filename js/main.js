ae9f0100c76edb065e4e86844b135be12fe553c7
osx04 <89367141+osx04@users.noreply.github.com>
2021-12-09 14:27:36 -0400

    Change breaks to interruptions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// Array with minutes on break
-var breaks = [];
-// Value used to generate ID for breaks
-var breaksIncrement = 0;
+// Array with minutes not working pallet [{id, min}]
+var interruptions = [];
+// Value used to generate ID for interruptions
+var interruptionIncrement = 0;
 
 // Calulcate pallet times
 function calculate(event) {
@@ -16,21 +16,21 @@ function calculate(event) {
     if (startTime > finishTime) {
         return alert('Start time must be earlier than finish time.')
     }
-    // Array of break minutes ex. [15, 60, 15]
-    var breaksArray = breaks.map(function (breakElement) {
-        return breakElement.min;
+    // Array of minutes not working pallet ex. [15, 60, 15]
+    var interruptionsArray = interruptions.map(function (interuption) {
+        return interuption.min;
     });
 
     // Add break minutes together into one number
-    var breakMinutes = breaksArray.reduce(function (acc, val) {
+    var interuptionMinutes = interruptionsArray.reduce(function (acc, val) {
         return acc + val;
     }, 0)
 
     // Break minutes converted to miliseconds
-    var breakMinutesInMs = breakMinutes * 60000;
+    var interruptionMinsInMs = interuptionMinutes * 60000;
 
     // Finish time - start time - time spent on break
-    var duration = finishTime - startTime - breakMinutesInMs;
+    var duration = finishTime - startTime - interruptionMinsInMs;
 
     // Get total hours from duration
     var hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
@@ -43,64 +43,56 @@ function calculate(event) {
     return false;
 }
 
-// // Change date to today keeping time the same
-// function changeDateToToday(date) {
-//     var todaysDate = new Date();
-//     date.setFullYear(todaysDate.getFullYear());
-//     date.setMonth(todaysDate.getMonth());
-//     date.setDate(todaysDate.getDate());
-// }
-
-function addBreak(event) {
+function addInterruption(event) {
     event.preventDefault();
 
-    // Prompt for break minutes
-    var breakMinutes = prompt('How long was your break? Please enter a number zero or greater.', 0);
-    var breakMinutesNum = parseInt(breakMinutes);
+    // Prompt for interruption minutes
+    var interruptionMinutes = prompt('How long were you away from your pallet? Please enter a number in minutes above zero.', 0);
+    var interruptionMinutesNum = parseInt(interruptionMinutes);
 
     // Validate number given by user
-    if (isNaN(breakMinutesNum)) {
+    if (isNaN(interruptionMinutesNum)) {
         alert('Please enter a valid number.');
         return false;
     }
 
-    if (breakMinutesNum < 0) {
+    if (interruptionMinutesNum < 0) {
         alert('Please enter a number zero or greater.');
         return false;
     }
 
-    // Add break to breaks array and increment next ID
-    breaks.push({id: breaksIncrement, min: breakMinutesNum});
-    breaksIncrement += 1;
-    updateBreaksTable();
+    // Add interruption to interruptions array and increment next ID
+    interruptions.push({id: interruptionIncrement, min: interruptionMinutesNum});
+    interruptionIncrement += 1;
+    updateInterruptionsTable();
 
     return false;
 }
 
-// Update breaks table
-function updateBreaksTable() {
-    var breaksList = document.getElementById('breaks-list');
+// Update interruptions table
+function updateInterruptionsTable() {
+    var interruptionList = document.getElementById('breaks-list');
 
-    if (breaks.length === 0) {
-        breaksList.setAttribute('class', 'hidden');
+    if (interruptions.length === 0) {
+        interruptionList.setAttribute('class', 'hidden');
     } else {
-        breaksList.removeAttribute('class')
+        interruptionList.removeAttribute('class')
     }
 
-    breaksList.innerHTML = breaks.map(function (value) {
-        return `<li id="${value.id}">${value.min} minutes <button onclick="return deleteBreak(event, '${value.id}')">Delete</button></li>`;
+    interruptionList.innerHTML = interruptions.map(function (value) {
+        return `<li id="${value.id}">${value.min} minutes <button onclick="return deleteInterruption(event, '${value.id}')">Delete</button></li>`;
     }).join('');
 }
 
-// Delete break from array and update table
-function deleteBreak(event, id) {
+// Delete interruption from array and update table
+function deleteInterruption(event, id) {
     event.preventDefault();
 
-    var breakToDelete = breaks.findIndex(function (breakElement) {
-        return breakElement.id == parseInt(id);
+    var interruptionToDelete = interruptions.findIndex(function (interupton) {
+        return interupton.id == parseInt(id);
     });
-    breaks.splice(breakToDelete, 1);
-    updateBreaksTable();
+    interruptions.splice(interruptionToDelete, 1);
+    updateInterruptionsTable();
 
     return false;
 }
